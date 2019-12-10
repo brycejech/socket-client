3be7af626110dbb173d7e670bfb6d8168bed9f1c
Bryce Jech <bryce@brycejech.com>
2019-12-10 12:37:25 -0600

    add vue app

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <h3>WebSocket Client App</h3>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+    components: { },
+})
+export default class App extends Vue {
+    
+}
+</script>
+
+<style lang="sass">
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+</style>
